e1c7e35a42f7aeb42957261d51358ba0c5be5693
normanrs <normanrs@live.com>
2019-01-16 18:08:15 -0700

    sortof

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-var min = 1
-var max = 100
+sessionStorage.setItem("minNum", 1);
+sessionStorage.setItem("maxNum", 100);
+sessionStorage.setItem("randNum", Math.floor(Math.random() * 100 ) + 1)
 
-function aMin(){
-  var min = document.getElementById('min').value;
+function aRange(){
+  let minIn = document.getElementById('minRange').value || 1;
+  let maxIn = document.getElementById('maxRange').value || 100;
+  sessionStorage.setItem("minNum", minIn);
+  sessionStorage.setItem("maxNum", maxIn);
+  sessionStorage.setItem("randNum", Math.floor(Math.random() * maxIn ) + minIn)
+  currentRange.innerHTML = ('Current range: ' + minIn + ' - ' + maxIn);
 }
-
-function aMax(){
-  var max = document.getElementById('max').value;
-}
-
-var randomNumber = Math.floor(Math.random() * max ) + min;
 
 function aGuess(){
+  let min = sessionStorage.getItem("minNum");
+  let max = sessionStorage.getItem("maxNum");
+  let randomNumber = sessionStorage.getItem("randNum");
   var guess = document.getElementById('guessInput').value;
   lastGuess.innerHTML = guess;
 
-  if (parseInt(guess) === randomNumber ) {
+  if ( guess === randomNumber ) {
     result.innerHTML = 'BOOM!';
   }
-  else if (parseInt(guess) > randomNumber){
-    result.innerHTML = 'Your guess is too high';
+  else if ( guess > randomNumber){
+    result.innerHTML = 'Your guess is too high' + randomNumber;
   }
-  else if (parseInt(guess) < randomNumber){
-    result.innerHTML = 'Your guess is too low';
+  else if ( guess < randomNumber){
+    result.innerHTML = 'Your guess is too low' + randomNumber;
   }
   else {
     result.innerHTML =  'Invalid entry';
@@ -34,16 +37,19 @@ function aClear(){
 }
 
 function aReset() {
+  sessionStorage.clear();
   location.reload();
 }
 
 function handleChange(input) {
-    if (input.value < 0) {lastNote.innerHTML = 'Input too low';
-      document.getElementById('guessInput').value = '';
-    }
-    if (input.value > 100) {lastNote.innerHTML = 'Input too high';
-      document.getElementById('guessInput').value = '';
-    }
+  let min = sessionStorage.getItem("minNum");
+  let max = sessionStorage.getItem("maxNum");
+  if (input.value < min.value ) {lastNote.innerHTML = 'Input too low ' + min;
+    document.getElementById('guessInput').value = '';
   }
-
-document.write('<p>Current range: ' + min + ' - ' + max + '</p>');
+  else if (input.value > max.value ) {lastNote.innerHTML = 'Input too high ' + max;
+    document.getElementById('guessInput').value = '';
+  }
+  else {lastNote.innerHTML = 'Good guess';
+  }
+}
