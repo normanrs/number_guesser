ea1c76e4d08269bdae599a06dab42e9de2220bad
normanrs <normanrs@live.com>
2019-01-20 12:27:17 -0700

    made more fun

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ function aRange(){
   minNum = parseInt(document.getElementById('minRange').value) || minNum;
   maxNum = parseInt(document.getElementById('maxRange').value) || maxNum;
   randNum = getRandom();
-  currentRange.innerHTML = ( 'Current range: ' + minNum + ' - ' + maxNum );
+  currentRange.innerHTML = ( 'Current range: ' + minNum + ' to ' + maxNum );
 }
 
 
 function aGuess(){
   let guess = parseInt(document.getElementById('guessInput').value);
-  currentRange.innerHTML = ( 'Current range: ' + minNum + ' - ' + maxNum );
+  currentRange.innerHTML = ( 'Current range: ' + minNum + ' to ' + maxNum );
   lastGuess.innerHTML = guess;
   enableBtn("resetBtn"); enableBtn("clearBtn");
   let feedback = evalGuess(guess, randNum);
@@ -25,8 +25,8 @@ function evalGuess(guess, winner) {
     randNum = getRandom();
     return 'BOOM! Range LevelUp!';
   }
-  else if ( guess > winner ){ return 'That is too high' + ' hint:' + randNum; }
-  else if ( guess < winner ){ return 'That is too low' + ' hint:' + randNum; }
+  else if ( guess > winner ){ return 'That is too high ' + randTry(); }
+  else if ( guess < winner ){ return 'That is too low ' + randTry(); }
   else { return 'Invalid entry'; }
 }
 
@@ -34,7 +34,7 @@ function levelUp(){
   let delta = 10
   minNum = (minNum - delta);
   maxNum = (maxNum + delta);
-  currentRange.innerHTML = ( 'Current range: ' + minNum + ' - ' + maxNum );
+  currentRange.innerHTML = ( 'Current range: ' + minNum + ' to ' + maxNum );
 }
 
 function getRandom() {
@@ -45,7 +45,30 @@ function handleChange(input) {
   let checkVal = parseInt(input.value)
   if ( checkVal < minNum ) { lastNote.innerHTML = 'INPUT TOO LOW'; }
   else if (checkVal > maxNum ) { lastNote.innerHTML = 'INPUT TOO HIGH'; }
-  else { lastNote.innerHTML = 'Good guess'; }
+  else { lastNote.innerHTML = 'accepted'; }
+}
+
+function randTry() {
+  const trySynonym = [' is worth a try..',
+             ' would be an attempt..',
+             ' is go..',
+             ' is the struggle..',
+             ' might work out..',
+             ' is looking good..',
+             ' would suffice..',
+             ' probably wins..',
+             ' is money..',
+             ' does the trick..',
+             ' floats my boat..',
+             ' gets the job done..',
+             ' is sensational..',
+             " that's the ticket..",
+             ' esta bien..',
+             ' might succeed..',
+             ' would not be crazy..',
+             ' and you have it..'
+           ];
+  return '(' + randNum + trySynonym[Math.floor(Math.random() * trySynonym.length)] + ')'
 }
 
 function disableBtn(button) {
